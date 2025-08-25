73bcda1fd8ec8e907eeba34c8632a326c1fcb52a
Ajay Katrapati <your_email_address@example.com>
2025-08-24 21:34:16 -0400

    l

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@db:5432/postgres"
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+        "SQLALCHEMY_DATABASE_URI",
+        "postgresql://postgres:postgres@db:5432/postgres"
+    )
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
